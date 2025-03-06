55c0e6c821f158e89c8c4b386ceca14c7ff348e4
zonde306 <anonymous@localhost>
2025-03-06 16:58:49 +0800

    fix: error

--- fal.py
+++ fal.py
@@ -12,18 +12,20 @@ import httpx_sse
 
 logger = logging.getLogger(__name__)
 
-CLIENTS : dict[str, fal_client.AsyncClient] = {}
-CLIENTS_POLL : int = -1
+CLIENTS: dict[str, fal_client.AsyncClient] = {}
+CLIENTS_POLL: int = -1
+
 
 def setup():
     for key in defines.API_KEYS:
         if k := key.strip():
             CLIENTS[k] = fal_client.AsyncClient(k)
-    
+
     if not CLIENTS:
         logger.info("No API keys available")
     else:
         logger.info(f"Using API keys: {len(CLIENTS)}")
+
 
 def next_client() -> fal_client.AsyncClient:
     if not CLIENTS:
@@ -35,7 +37,10 @@ def next_client() -> fal_client.AsyncClient:
     clients = list(CLIENTS.values())
     return clients[CLIENTS_POLL % len(clients)]
 
-async def format_messages(messages: list[dict], role_info : features.RoleInfo) -> tuple[str, str]:
+
+async def format_messages(
+    messages: list[dict], role_info: features.RoleInfo
+) -> tuple[str, str]:
     processed = collections.defaultdict(str)
     for i, msg in enumerate(messages):
         contents = []
@@ -45,22 +50,24 @@ async def format_messages(messages: list[dict], role_info : features.RoleInfo) -
                     contents.append(cont)
         else:
             contents.append(msg["content"])
-        
+
         for cont in contents:
             if "<|removeRole|>" in cont:
-                cont = cont.replace("<|removeRole|>\n", "").replace("<|removeRole|>", "")
+                cont = cont.replace("<|removeRole|>\n", "").replace(
+                    "<|removeRole|>", ""
+                )
             else:
-                role : str = msg.get("role", "")
+                role: str = msg.get("role", "")
                 role = getattr(role_info, role.lower(), role_info.system)
                 cont = f"\b{role}:{cont}"
-            
+
             cont = re.sub(r"\n{2,}", r"\n", cont).strip()
             cont = re.sub(r"(^[ \u3000]+|[ \u3000]+$)", "", cont)
             processed[i] += cont + "\n"
-        
+
         processed[i] = processed[i].strip()
 
-    processed = [ processed[i] for i in sorted(processed.keys()) ]
+    processed = [processed[i] for i in sorted(processed.keys())]
 
     full = False
     output1, output2 = "", ""
@@ -73,18 +80,25 @@ async def format_messages(messages: list[dict], role_info : features.RoleInfo) -
                 output2 = prompt + "\n" + output2
             else:
                 full = True
-        
+
         if full:
             if first_length + len(output1) + len(prompt) < defines.PROMPT_CHARS_LIMIT:
                 output2 = prompt + "\n" + output2
             else:
                 break
-    
+
     output1 = first_message + "\n" + output1
     return output1.strip(), output2.strip()
 
-async def send_message(messages: list[dict], api_key: str, model : str, reasoning : bool = False) -> AsyncGenerator[dict, None]:
-    client = CLIENTS.get(api_key, fal_client.AsyncClient(api_key)) if api_key else next_client()
+
+async def send_message(
+    messages: list[dict], api_key: str, model: str, reasoning: bool = False
+) -> AsyncGenerator[dict, None]:
+    client = (
+        CLIENTS.get(api_key, fal_client.AsyncClient(api_key))
+        if api_key
+        else next_client()
+    )
 
     feat = features.process_features(messages)
     prompt1, prompt2 = await format_messages(messages, feat.ROLE)
@@ -96,13 +110,16 @@ async def send_message(messages: list[dict], api_key: str, model : str, reasonin
     print("Response:")
 
     try:
-        assert len(prompt1) < defines.PROMPT_CHARS_LIMIT and len(prompt2) < defines.PROMPT_CHARS_LIMIT, f"Prompt too long: {len(prompt1)} + {len(prompt2)}"
+        assert (
+            len(prompt1) < defines.PROMPT_CHARS_LIMIT
+            and len(prompt2) < defines.PROMPT_CHARS_LIMIT
+        ), f"Prompt too long: {len(prompt1)} + {len(prompt2)}"
 
         handler = client.stream(
             "fal-ai/any-llm",
             arguments={
                 "system_prompt": prompt1,
-                "prompt" : prompt2,
+                "prompt": prompt2,
                 "reasoning": reasoning,
                 "model": model,
             },
@@ -115,7 +132,7 @@ async def send_message(messages: list[dict], api_key: str, model : str, reasonin
             if event["error"]:
                 error_message = event["error"]
                 break
-            
+
             # 为什么输出流里会包含上次的输出？
             if event["reasoning"] and len(event["reasoning"]) >= prev_reasoning:
                 event["reasoning"] = event["reasoning"][prev_reasoning:]
@@ -123,7 +140,7 @@ async def send_message(messages: list[dict], api_key: str, model : str, reasonin
             if event["output"] and len(event["output"]) >= prev_output:
                 event["output"] = event["output"][prev_output:]
                 prev_output += len(event["output"])
-            
+
             if reasoning and not is_reasoning and event["reasoning"]:
                 is_reasoning = True
                 content = f"<thinking>\n{event['reasoning']}"
@@ -134,18 +151,20 @@ async def send_message(messages: list[dict], api_key: str, model : str, reasonin
                 content = event["reasoning"]
             else:
                 content = event["output"]
-            
+
             yield {
-                "id" : request_id,
+                "id": request_id,
                 "object": "chat.completion.chunk",
                 "created": int(time.time()),
                 "model": model,
-                "choices": [{
-                    "index": 0,
-                    "delta": {
-                        "content": content,
-                    },
-                }],
+                "choices": [
+                    {
+                        "index": 0,
+                        "delta": {
+                            "content": content,
+                        },
+                    }
+                ],
             }
             print(content, end="")
     except (fal_client.client.FalClientError, AssertionError) as e:
@@ -153,78 +172,90 @@ async def send_message(messages: list[dict], api_key: str, model : str, reasonin
         logger.error(f"Error: {e}", exc_info=True)
     except httpx_sse._exceptions.SSEError as e:
         ...
-	
-	# just a \n
-	print("")
-	
+
+    # just a \n
+    print("")
+
     if error_message:
         print(f"ERROR: {error_message}")
         yield {
-            "id" : request_id,
+            "id": request_id,
             "object": "chat.completion.chunk",
             "created": int(time.time()),
             "model": model,
-            "choices": [{
-                "index": 0,
-                "delta": {
-                    "content": f"ERROR: {error_message}",
-                },
-                "finish_reason": "error",
-            }],
+            "choices": [
+                {
+                    "index": 0,
+                    "delta": {
+                        "content": f"ERROR: {error_message}",
+                    },
+                    "finish_reason": "error",
+                }
+            ],
         }
     else:
         yield {
-            "id" : request_id,
+            "id": request_id,
             "object": "chat.completion.chunk",
             "created": int(time.time()),
             "model": model,
-            "choices": [{
-                "index": 0,
-                "delta": {
-                    "content": "",
-                },
-                "finish_reason": "stop",
-            }],
+            "choices": [
+                {
+                    "index": 0,
+                    "delta": {
+                        "content": "",
+                    },
+                    "finish_reason": "stop",
+                }
+            ],
         }
 
-async def send_message_sync(messages: list[dict], api_key: str, model : str, reasoning : bool = False) -> dict:
+
+async def send_message_sync(
+    messages: list[dict], api_key: str, model: str, reasoning: bool = False
+) -> dict:
     content = ""
     error_message = ""
     async for message in send_message(messages, api_key, model, reasoning):
         content += message["choices"][0]["delta"]["content"]
         if message["choices"][0]["finish_reason"] == "error":
             error_message = message["choices"][0]["delta"]["content"]
-    
+
     if error_message:
         return {
-            "id" : message["id"],
+            "id": message["id"],
             "object": "chat.completion",
             "created": message["created"],
             "model": model,
-            "choices": [{
-                "index": 0,
-                "message": {
-                    "role": "assistant",
-                    "content": error_message,
-                },
-                "finish_reason": "error",
-            }],
+            "choices": [
+                {
+                    "index": 0,
+                    "message": {
+                        "role": "assistant",
+                        "content": error_message,
+                    },
+                    "finish_reason": "error",
+                }
+            ],
         }
-    
+
     return {
-        "id" : message["id"],
+        "id": message["id"],
         "object": "chat.completion",
         "created": message["created"],
         "model": model,
-        "choices": [{
-            "index": 0,
-            "message": {
-                "role": "assistant",
-                "content": content,
-            },
-            "finish_reason": "stop",
-        }],
+        "choices": [
+            {
+                "index": 0,
+                "message": {
+                    "role": "assistant",
+                    "content": content,
+                },
+                "finish_reason": "stop",
+            }
+        ],
         "usage": None,
     }
+
 
 setup()
